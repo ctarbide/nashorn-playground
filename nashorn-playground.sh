f533c7e1453149c72709cb5d7377f06a86ba9cf0
Cungsten Tarbide <ctarbide@tuta.io>
2021-05-23 11:15:03 -0300

    minor repl improvements and add bouncycastle and commons-codec dependencies

--- nashorn-playground.sh
+++ nashorn-playground.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -eu
 thispath=`perl -MCwd=realpath -le'print(realpath(\$ARGV[0]))' -- "${0}"`
-thisprog=${thispath##*/}
-. "${thispath%*.sh}/config.inc.sh"
-classpath_file=${thispath%*.sh}/mdep.classpath
-target_classes=${thispath%*.sh}/classes
-PATH="${JAVA_HOME}/bin:${PATH}" CLASSPATH="${target_classes}:`cat "${classpath_file}"`" \
+if [ -d "${thispath%*.sh}" ]; then
+    dir=${thispath%*.sh}
+else
+    dir=${thispath%/*}/target
+fi
+. "${dir}/config.inc.sh"
+PATH="${JAVA_HOME}/bin:${PATH}" CLASSPATH="${dir}/classes:`cat "${dir}/mdep.classpath"`" \
     exec java org.openjdk.nashorn.tools.Shell "$@"
